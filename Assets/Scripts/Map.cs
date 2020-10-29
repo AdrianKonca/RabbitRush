51ef62d508880a74332dd2dbec81444d6b932db9
AdrianKonca <48868811+AdrianKonca@users.noreply.github.com>
2020-10-29 17:32:28 +0100

    Adds really basic map for character testing

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = 0; x < 5; x++)
+        {
+            for (int z = 0; z < 5; z++)
+            {
+                var gameObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                gameObject.name = "Tile(" + x + ", " + z + ")";
+                gameObject.transform.parent = this.transform;
+                gameObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                gameObject.transform.position = new Vector3((float)x + 0.5f, 0f, (float)z + 0.5f);
+
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
