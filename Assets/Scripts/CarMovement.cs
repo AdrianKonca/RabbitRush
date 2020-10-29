b9bf9e3cb97126444144f45d0c518d18a5e922b3
AdrianKonca <48868811+AdrianKonca@users.noreply.github.com>
2020-10-29 22:27:46 +0100

    Adds basic entity spawner and first entity to the game.

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public float MovementSpeed = 0.05f;
+    public float DirectionAngle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += this.transform.forward * MovementSpeed * Time.deltaTime;
+        //var vForce = Quaternion.AngleAxis(DirectionAngle, Vector3.forward) * Vector3.right + new Vector3(0f, 0.5f, 0f);
+        //Debug.DrawLine(this.transform.position, vForce);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Prefab;
+    public float Frequency;
+    public int EntityLimit = 10;
+    private float _lastSpawned;
+    private Queue<GameObject> _prefabs;
+
+    private void Spawn()
+    {
+        var newPrefab = Instantiate(Prefab, this.transform);
+        _prefabs.Enqueue(newPrefab);
+        if (_prefabs.Count > EntityLimit)
+            Destroy(_prefabs.Dequeue());
+        _lastSpawned = Time.time;
+    }
+    void Start()
+    {
+        _prefabs = new Queue<GameObject>();
+        Spawn();
+    }
+
+    void Update()
+    {
+        if (Time.time > _lastSpawned + Frequency)
+        {
+            Spawn();
+        }
+    }
+}
